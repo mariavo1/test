9e5d539069dcece8e576324b933f03c3375bbb72
ArmaniLeSure <54874404+ArmaniLeSure@users.noreply.github.com>
2021-11-29 15:37:57 -0600

    Deleted lines

--- app/src/main/java/com/example/tempconverter/TETSDT.java
+++ app/src/main/java/com/example/tempconverter/TETSDT.java
@@ -2,11 +2,5 @@ package com.example.tempconverter;
 
 public class TETSDT {
 
-    //Super stinky chili crockpot
-    //fandango
-    /*gugyguuiuehwwifew;iFbqwf
-    qwfqwuifqwbufqfbqwiubfqwbfuqwbF
-            W{Q)fbuifdaWbufa;bufawbufa}
-            FfiaBFUIFBiuwbfuIBFIbfefes
-    febuigrgbsegbseiugsibguse*/
+
 }
